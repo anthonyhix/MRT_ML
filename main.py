7655cbea2ce9e75ede5ce3d83585dc9b6306d405
Anthony Hix <anthonymhix@outlook.com>
2024-04-04 20:48:26 -0400

    Modified processor

--- main.py
+++ main.py
@@ -15,23 +15,21 @@ scores_csv = np.loadtxt('training_data/scores.csv', delimiter=',', dtype = 'S')
 
 # for each row in the scores_csv, the first argument is the in.wav path, and the second is the out.wav, and the third is the score. Store in separate vectors:
 scores_len = scores_csv.shape[0]
-path_in = []
-path_out = []
+path = []
 scores = []
 for i in range(scores_len):
-    path_in = np.append(path_in, str(scores_csv[i][0].decode()))
-    path_out = np.append(path_out, str(scores_csv[i][1].decode()))
-    scores = np.append(scores, float(scores_csv[i][2]))
+    path = np.append(path, str(scores_csv[i][0].decode()))
+    scores = np.append(scores, float(scores_csv[i][1]))
 
 # Preprocess the input and output wav files
 # data_x is a 2D array where each row is the input and output wav files concatenated in the frequency domain
 # data_y is a 1D array where each element is the score of the corresponding row in data_x
 
-data_x = [np.append(pp.preprocess_wav(librosa.load('training_data/' + path_in[0])), pp.preprocess_wav(librosa.load('training_data/' + path_out[0])))]
+data_x = [pp.preprocess_wav(librosa.load('training_data/' + path[0]))]
 data_y = [[scores[0]]]
 
 for j in range(1, scores_len):
-    data_x = np.append(data_x, [np.append(pp.preprocess_wav(librosa.load('training_data/' + path_in[j])), pp.preprocess_wav(librosa.load('training_data/' + path_out[j])))], axis = 0)
+    data_x = np.append(data_x, [pp.preprocess_wav(librosa.load('training_data/' + path[j]))], axis = 0)
     data_y = np.append(data_y, [[scores[j]]], axis = 0)
 
 # Normalize the data
