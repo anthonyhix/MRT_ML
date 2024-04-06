600fb15f106e3bad3b6e53382ea1a547b3ad263b
Anthony Hix <anthonymhix@outlook.com>
2024-04-06 00:26:58 -0400

    Added all training data.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import preprocessor as pp
 
 #normalize with min/max
 def normalize(data):
-    return (data - np.min(data)) / (np.max(data) + 1 - np.min(data))
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
 
 # Load the training data from training_data folder
 scores_csv = np.loadtxt('training_data/scores.csv', delimiter=',', dtype = 'S')
@@ -32,7 +32,7 @@ data_y = [[scores[0]]]
 labels = [0, 1]
 
 for j in range(1, scores_len):
-    data_x = np.append(data_x, [pp.preprocess_wav(librosa.load('training_data/' + path[j]))], axis = 0)
+    data_x = np.append(data_x, [pp.preprocess_wav(librosa.load('training_data/' + path[j]), end_offset = 1)], axis = 0)
     data_y = np.append(data_y, [[scores[j]]], axis = 0)
 
 # Normalize the input data
@@ -41,23 +41,27 @@ data_x = normalize(data_x)
 # create sequential model
 model = keras.Sequential()
 model.add(layers.Input(shape=(data_x.shape[1],)))
-model.add(layers.Dense(526, activation='linear'))
-model.add(layers.Dense(128, activation='swish'))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(16, activation='swish'))
+model.add(layers.Dense(526, activation = 'relu'))
+model.add(layers.Dense(128))
+model.add(layers.Dense(64))
+model.add(layers.Dense(16))
 model.add(layers.Dense(2))
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+#loss_fn = tf.keras.losses.MeanSquaredError()
 
 model.compile(optimizer='adam', loss = loss_fn, metrics=['accuracy'])
 
-history = model.fit(data_x, data_y, epochs=10)
+history = model.fit(data_x, data_y, epochs=40)
 
-test_loss, test_acc = model.evaluate(data_x, data_y, verbose=2)
+test_loss, test_acc = model.evaluate(data_x, data_y, verbose=1)
+
+print('\nTest accuracy:', test_acc)
 
 model.summary()
 
 print(history.history)
 
 print('\nReal Results:', data_y)
-print(model.predict(data_x))
+model.add(layers.Softmax())
+print('\nEstimated Results:', model.predict(data_x))
--- preprocessor.py
+++ preprocessor.py
@@ -2,12 +2,11 @@
 
 import numpy as np
 
-def preprocess_wav(y, freq_min = 1000, freq_max = 7000, time_min = 0, time_max = float('inf')):
+def preprocess_wav(y, freq_min = 1000, freq_max = 7000, end_offset = 5):
     # Process the wav file with an FFT
     sr = y[1]
     y = y[0]
-    sample_min = int(time_min * sr)
-    sample_max = int(np.min([ time_max * sr, len(y)]))
+    sample_min = np.min([0, int(end_offset*sr)])
     freq_max = np.min([freq_max, sr])
-    out = np.fft.rfft(y[sample_min:sample_max])
+    out = np.fft.rfft(y[sample_min:])
     return out[freq_min:freq_max].real
