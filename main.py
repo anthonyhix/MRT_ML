8bde0aefb966e6586042b5cfaf5e9da12e4e98b1
Anthony Hix <anthonymhix@outlook.com>
2024-04-23 10:36:36 -0400

    Updated model metrics, added better data splitting logic, created validation figures.

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ import matplotlib.pyplot as plt
 import preprocessor as pp
 import sklearn
 from sklearn.feature_selection import SelectKBest, f_classif, chi2
+from sklearn.model_selection import train_test_split
 
 
 # Function to normalize data between 0 and 1 by column
 def normalize(data):
-    min_vals = np.min(data, axis=0)
-    max_vals = np.max(data, axis=0)
-    data = (data - min_vals) / (max_vals - min_vals)
-    return data
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0)
+    return (data - mean) / std
+
 
 
 # Load and preprocess data
@@ -28,6 +29,7 @@ path = scores_csv[indices, 0].astype(str)
 scores = scores_csv[indices, 1].astype(float)
 freq_min = 1000
 freq_max = 7000
+load_amt = 3500
 
 # Preprocess wav files
 # Preprocess wav files
@@ -35,18 +37,19 @@ data_x = []
 data_y = []
 group_size = 1
 wav_file = librosa.load('training_data/' + path[0])
-preprocessed_wav = pp.preprocess_wav(wav_file, sample_time = 0.5, group_size = group_size)
+preprocessed_wav = pp.preprocess_wav(wav_file, sample_time = 1, group_size = group_size)
 data_x.append(preprocessed_wav)
 data_y.append(scores[0])
-for i in range(1, scores_len):
+for i in range(1, min([load_amt,scores_len])):
     wav_file = librosa.load('training_data/' + path[i])
-    preprocessed_wav = pp.preprocess_wav(wav_file, sample_time = 0.5, group_size = group_size)
+    preprocessed_wav = pp.preprocess_wav(wav_file, sample_time = 1, group_size = group_size)
     if len(preprocessed_wav) != len(data_x[0]):
         print('Error: Incorrect length of preprocessed wav file')
         continue
     data_x.append(preprocessed_wav)
     # Get the score for the wav file
     data_y.append(scores[i])
+    print('Data Loaded: ' + str(i + 1) + '/' + str(min([load_amt,scores_len])))
 data_x = np.stack(data_x)
 data_y = np.stack(data_y)
 
@@ -58,7 +61,7 @@ plt.figure()
 
 
 # Normalize the input data
-#data_x = normalize(data_x)
+data_x = normalize(data_x)
 
 # Compute F score for each feature
 
@@ -71,7 +74,7 @@ plt.ylabel('F score')
 plt.figure()
 
 # Select the top n features
-selector = SelectKBest(f_classif, k=75)
+selector = SelectKBest(f_classif, k=350)
 
 # Fit the selector to the training data
 selector.fit(data_x, data_y.ravel())
@@ -79,32 +82,40 @@ selector.fit(data_x, data_y.ravel())
 # Transform the training and test data
 data_x = selector.transform(data_x)
 
+# Create a healthy split of data
+combined = np.column_stack((data_x, data_y))
+
+train, test = train_test_split(combined, test_size=0.2)
 
 # Split data into training and test sets
-training_batch_size = 3000
-test_batch_size = 500
-training_data_x = data_x[:training_batch_size, :]
-training_data_y = data_y[:training_batch_size]
-test_data_x = data_x[training_batch_size:, :]
-test_data_y = data_y[training_batch_size:]
+training_data_x = train[:, :-1]
+training_data_y = train[:, -1].reshape(-1, 1)
+test_data_x = test[:, :-1]
+test_data_y = test[:, -1].reshape(-1, 1)
 
 # Create and compile model
 model = keras.Sequential([
-    layers.Input(shape=(data_x.shape[1],)),
-    layers.Dense(64, activation = 'relu'),
-    layers.Dropout(0.3),
+    layers.Input(shape=(training_data_x.shape[1],)),
+    layers.Dense(24, activation = 'relu'),
+    layers.Dense(16, activation = 'relu'),
     layers.Dense(4, activation = 'relu'),
     layers.Dense(1, activation = 'sigmoid')
 ])
 
 threshold = 0.5
 
-binary_metric = tf.keras.metrics.BinaryIoU(threshold=threshold)
+metrics = [
+      # keras.metrics.BinaryCrossentropy(name='cross entropy'),  # same as model's loss
+      # keras.metrics.MeanSquaredError(name='Brier score'),
+      # keras.metrics.FalsePositives(name='fp'),
+      keras.metrics.FalseNegatives(name='fn'), 
+      # keras.metrics.BinaryAccuracy(name='accuracy'),
+]
 optimizer = tf.keras.optimizers.Adam()
 
 model.compile(optimizer=optimizer, 
              loss='binary_crossentropy',
-              metrics=['binary_accuracy'])
+              metrics=metrics)
 
 # Train model
 history = model.fit(training_data_x, training_data_y, epochs=100, validation_split = 0.1)
@@ -148,4 +159,25 @@ plt.title('Model loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='lower left')
+plt.figure()
+
+counts, bins = np.histogram(prediction_model.predict(test_data_x), bins=20)
+plt.hist(bins[:-1], bins, weights=counts)
+plt.xlabel('Score')
+plt.ylabel('Frequency')
+plt.figure()
+
+print('Ratio of Likely Positives to Likely Negatives (95% Interval): ' + str(counts[-1]/counts[0]))
+print('Ratio of Actual Positives to Actual Negatives (95% Interval): ' + str(np.sum(data_y)/(len(data_y) - np.sum(data_y))))
+
+
+counts, bins = np.histogram(prediction_model.predict(test_data_x), bins=10)
+plt.hist(bins[:-1], bins, weights=counts)
+plt.xlabel('Score')
+plt.ylabel('Frequency')
+
+print('Ratio of Likely Positives to Likely Negatives (90% Interval): ' + str(counts[-1]/counts[0]))
+print('Ratio of Actual Positives to Actual Negatives (90% Interval): ' + str(np.sum(data_y)/(len(data_y) - np.sum(data_y))))
 plt.show()
+
+
